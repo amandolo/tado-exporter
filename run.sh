5f9b74139ed3587114150603017649ff99f47432
Andrea Mandolo <andrea.mandolo@gmail.com>
2024-10-30 01:23:27 +0100

    [fix] new APIs adoption

---
+++ run.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+export EXPORTER_PASSWORD='0RMgBlMQLsfQta6dFEC5!poIp'
+export EXPORTER_USERNAME="andrea.mandolo@gmail.com"
+export EXPORTER_CLIENT_SECRET="wZaRN7rpjn3FoNyF5IFuxg9uMzYJcvOoQ8QWiIqS3hfk6gLhVlG57j5YNoZL2Rtc"
+
+cargo run
